c1d94304d03445c09bf4eb324b577ba09fbfeefa
Christoph Beeler <christoph.beeler@viega.ch>
2025-12-19 19:18:31 +0100

    removing sleep for now

--- PDM_MIC_EQ.ino
+++ PDM_MIC_EQ.ino
@@ -100,9 +100,9 @@ void loop()
         BLE.disconnect();
         BLE.end();
 
-        delay(20000);
+        /*delay(20000);
         NRF_POWER->SYSTEMOFF = 1;
-        while (1) {};
+        while (1) {};*/
       }
       lowBattery = true;
     }
